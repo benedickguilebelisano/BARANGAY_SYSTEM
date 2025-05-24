a4735bb39fcf2e763a9f97cee96466a20cce5975
benedickguilebelisano <benedick2026@gmail.com>
2025-05-25 00:41:16 +0800

    Initial push

--- src/main/java/bsp/MainActivity.java
+++ src/main/java/bsp/MainActivity.java
@@ -7,17 +7,30 @@ public class MainActivity {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("helllo world");
 		
-		while (true) {
-			System.out.println("Welcome Shonen");
-			System.out.println("1.Login");  //number are temporary for console
-			System.out.println("2.Registration");
-
-			int choice = scanner.nextInt(); scanner.nextLine();
-			switch (choice) {
-				case 1 -> //to be implemented to get data in database&System(scanner);
-				case 2 -> //(scanner);
-				default -> System.out.println("sdas");
+		do{
+			//Efficient for non-hard coding 
+			String arr[] = {"Login", "Registration"};
+			
+			for(int i = 0; i<arr.length; i++) {
+				System.out.println((i+1) + ". " + arr[i]);
 			}
-		}
+			
+			try {
+				int choice = scanner.nextInt(); scanner.nextLine();
+				switch (choice) {
+					case 1:
+						System.out.println("Welcome Shonen");
+						//
+						break;//to be implemented to get data in database&System(scanner);
+					case 2:
+						System.out.println("o");
+						break;//(scanner);
+					default: System.out.println("sdas");
+				}
+			}catch (Exception e) {
+				System.out.println("Invalid Output.");
+				scanner.next();
+			}
+		}while(true);
 	}
 }
